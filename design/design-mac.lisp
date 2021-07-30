c3ba22910c849cb20b46f1e9cfa658fa813e2411
sazare <song.of.sand@gmail.com>
2021-07-30 19:56:12 +0900

    lispify html functions in design-mac.lisp
    	new file:   design/design-mac.lisp
    	modified:   design/design1.ch1

--- design/design-mac.lisp
+++ design/design-mac.lisp
@@ -1,5 +1,50 @@
 ;; a sample code
 
+;; utility 
+(defun writeafile (fname objects)
+  (with-open-file (out fname
+      :direction :output
+      :if-exists :supersede)
+
+    (format out objects)
+  )
+)
+
+;; primitives
+(defun tag (name content) 
+  (format nil "<~a>~a</~a>" name content name)
+)
+
+(defun html(title)
+  (tag 'html 
+    (my-title title)
+  )
+)
+
+
+(defun meta (&rest attr)
+  (loop with mm = "<meta "
+    for np on attr by #'cddr do
+      (setq mm (format nil "~a ~a=~a" mm (car np)(cadr np)))
+    finally
+      (return (format nil "~a>" mm))
+  )
+)
+
+;; sample
+
+;<meta charset="UTF-8">
+;<meta http-equiv="X-UA-Compatible" content="IE=edge">
+;<meta name="format-detection" content="telephone=no">
+;<meta name="viewport" content="width=device-width">
+(meta :charset "UTF-8")
+(meta :http-equiv "X-UA-Compatible" :content "IE=edge")
+(meta :name "format-detection" :content "telephone=no")
+(meta :name "viewport" :content="width device-width")
+
+
+
+;; 
 (defun my-meta ()
   "<meta charset=\"utf-8\">"
 )
@@ -50,7 +95,7 @@
 (defun my-doc (mytitle)
   (let (d1 d2 html)
     (my-head "CSSとHTMLの例")
-    (setq d1 (make-div :line1 "これはすばらしい" :cname :ja))
+    (setq d1 (make-div :line1 "これは最悪" :cname :ja))
     (setq d2 (make-div :line2 "ファンタスティック" :cname :eja))
 
     (setq html (my-page 
@@ -63,14 +108,6 @@
 )
 
 
-(defun writeafile (fname objects)
-  (with-open-file (out fname
-      :direction :output
-      :if-exists :supersede)
-
-    (format out objects)
-  )
-)
 
 (defparameter p1 (my-doc "CSSとHTMLの例"))
 (writeafile "my.html" p1)
