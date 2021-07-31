ded68cddf6baa1f19b0b00f145509ed7a4e72143
sazare <song.of.sand@gmail.com>
2021-07-31 17:09:25 +0900

    整理した
    	modified:   design/design-mac.lisp

--- design/design-mac.lisp
+++ design/design-mac.lisp
@@ -21,6 +21,7 @@
 )
 
 ;; tag layer
+;; pattern <tag a=b c=d ...>f* g* h*</tag>
 (defun tag (name attrs &rest contents) 
   (let ((ats (attributes attrs)))
     (format nil "<~a~a>~{~a ~}</~a>" name ats contents name)
@@ -39,29 +40,21 @@
 
 (tagn :div '(:class "foo" :id "Neko") '("alphabeta" "<p>hello</>"))
 
-
+;; pattern: <tag a=b c=d ...> 
 (defun tag1 (name attrs)
-  (format nil "<~a~a>" name (attributes attrs))
+  (format nil "<~a~a/>" name (attributes attrs))
 )
 
 
-;; html layer
-(defun html(title)
-  (tag 'html () (my-title title)
-  )
-)
+;; HTML layer
+
 (defun meta (&rest attrs)
   (tag1 :meta attrs)
 )
 
-(defun link (attrs)
+(defun link (&rest attrs)
   (tag1 :link attrs)
 )
-
-;(defun meta0 (&rest attr)
-;  (format nil "<meta ~a>" (attributes attr))
-;)
-;; sample
 
 ;<meta charset="UTF-8">
 ;<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -71,17 +64,17 @@
 (meta :http-equiv "X-UA-Compatible" :content "IE=edge")
 (meta :name "format-detection" :content "telephone=no")
 (meta :name "viewport" :content "width device-width")
-(meta "name" "viewport" "content" "width device-width")
+(meta :name "viewport" :content "width device-width")
 
 
-
-;;  customize
-(defun my-meta ()
+;;  user layer lower
+(defun my-utf8 ()
   (meta :charset "utf-8")
 )
 
+;??
 (defun my-style (file)
-  (tag1 :link (list :rel "stylesheet" :href file))
+  (link :rel "stylesheet" :href file)
 )
 
 (defun my-title (title)
@@ -90,57 +83,47 @@
 
 (defun my-head (title)
   (tag :head ()
-    (my-meta)
+    (my-utf8)
     (my-style "sample.css")
     (my-title title)
   )
 )
 
-(defun make-html (head body)
+(defun my-page (head body)
   (tag :html () head body)
 )
 
-(defun my-page (head body)
-  (make-html head body)
-)
-
-(defun make-body (&rest ds)
+(defun my-body (&rest ds)
   (tagn :body  () ds )
 )
 
-;; usage1 (make-div :abb "hello")
-;; usage2 (make-div 'aaa "hello2")
-;; usage3 (make-div :xyz "good" :cname "boojum")
-;; usage4 (make-div :zzz "bad" :cname "stark")
+;; usage1 (my-div0 :abb "hello")
+;; usage2 (my-div0 'aaa "hello2")
+;; usage3 (my-div0 :xyz "good" :cname "boojum")
+;; usage4 (my-div0 :zzz "bad" :cname "stark")
 
-(defun make-div0 (id text &rest attrs)
+(defun my-div0 (id text &rest attrs)
   (tag :div (append (list :id id) attrs) text)
 )
 
-;(defun make-div (id text &rest attrs)
-;  (let ((aid (intern (string id))))
-;    (if cname (set aid (format nil "<div id=\"~a\" class=\"~a\">~a</div>" aid cname text))
-;              (set aid (format nil "<div id=\"~a\">~a</div>" aid text)))
-;  )
-;)
 
+;;  user top layer 
 (defun my-doc (mytitle)
   (let (d1 d2 html)
     (my-head "CSSとHTMLの例")
-    (setq d1 (make-div0 :line1 "これは最悪" :class "ja"))
-    (setq d2 (make-div0 :line2 "ファンタスティック" :class "eja"))
+    (setq d1 (my-div0 :line1 "これは最悪" :class "ja"))
+    (setq d2 (my-div0 :line2 "ファンタスティック" :class "eja"))
 
     (setq html (my-page 
                   (my-head mytitle)
-                  (make-body d1 d2 d1)
+                  (my-body d1 d2 d1) ;; this causes 2 same ids.
                )
     )
     html
   )
 )
 
-
-
+;; doit
 (defparameter p1 (my-doc "CSSとHTMLの例"))
 (writeafile "my.html" p1)
 
