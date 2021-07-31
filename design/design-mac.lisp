ca0202a5cb125eb571fdcbb0ce7dc04ec09f63d5
sazare <song.of.sand@gmail.com>
2021-07-31 09:58:04 +0900

    made 3 layers pattern.   work in simple case(design-mac.lisp) but in more complex?

--- design/design-mac.lisp
+++ design/design-mac.lisp
@@ -11,26 +11,56 @@
 )
 
 ;; primitives
-(defun tag (name content) 
-  (format nil "<~a>~a</~a>" name content name)
-)
-
-(defun html(title)
-  (tag 'html 
-    (my-title title)
-  )
-)
-
-
-(defun meta (&rest attr)
-  (loop with mm = "<meta "
-    for np on attr by #'cddr do
-      (setq mm (format nil "~a ~a=~a" mm (car np)(cadr np)))
+(defun attributes (attrs)
+  (loop with mm = ""
+    for np on attrs by #'cddr do
+      (setq mm (format nil "~a ~a=\"~a\"" mm (car np)(cadr np)))
     finally
-      (return (format nil "~a>" mm))
+      (return mm)
   )
 )
 
+;; tag layer
+(defun tag (name attrs &rest contents) 
+  (let ((ats (attributes attrs)))
+    (format nil "<~a~a>~{~a ~}</~a>" name ats contents name)
+  )
+)
+
+(defun tagn (name attrs contents)
+  (let ((ats (attributes attrs)))
+    (format nil "<~a~a>~{~a ~}</~a>" name ats contents name)
+  )
+)
+
+;<div class="foo" id="Neko">alphabeta</div>
+(tag :div '(:class "foo" :id "Neko") "alphabeta")
+(tag :div '(:class "foo" :id "Neko") "alphabeta" "<p>hello</>")
+
+(tagn :div '(:class "foo" :id "Neko") '("alphabeta" "<p>hello</>"))
+
+
+(defun tag1 (name attrs)
+  (format nil "<~a~a>" name (attributes attrs))
+)
+
+
+;; html layer
+(defun html(title)
+  (tag 'html () (my-title title)
+  )
+)
+(defun meta (&rest attrs)
+  (tag1 :meta attrs)
+)
+
+(defun link (attrs)
+  (tag1 :link attrs)
+)
+
+;(defun meta0 (&rest attr)
+;  (format nil "<meta ~a>" (attributes attr))
+;)
 ;; sample
 
 ;<meta charset="UTF-8">
@@ -40,25 +70,26 @@
 (meta :charset "UTF-8")
 (meta :http-equiv "X-UA-Compatible" :content "IE=edge")
 (meta :name "format-detection" :content "telephone=no")
-(meta :name "viewport" :content="width device-width")
+(meta :name "viewport" :content "width device-width")
+(meta "name" "viewport" "content" "width device-width")
 
 
 
-;; 
+;;  customize
 (defun my-meta ()
-  "<meta charset=\"utf-8\">"
+  (meta :charset "utf-8")
 )
 
 (defun my-style (file)
-  (format nil "<link rel=\"stylesheet\" href=\"~a\">" file)
+  (tag1 :link (list :rel "stylesheet" :href file))
 )
 
 (defun my-title (title)
-  (format nil "<title>~a</title>" title)
+  (tag :title ()  title)
 )
 
 (defun my-head (title)
-  (format nil "<head>~a~a~a</head>"
+  (tag :head ()
     (my-meta)
     (my-style "sample.css")
     (my-title title)
@@ -66,7 +97,7 @@
 )
 
 (defun make-html (head body)
-  (format nil "<html>~a~a</html>" head body)
+  (tag :html () head body)
 )
 
 (defun my-page (head body)
@@ -74,29 +105,30 @@
 )
 
 (defun make-body (&rest ds)
-  (loop with body = "<body>" for di in ds do
-    (setq body (format nil "~a ~a" body di))
-    finally (return (format nil "~a </body>" body))
-  )
+  (tagn :body  () ds )
 )
-
 
 ;; usage1 (make-div :abb "hello")
 ;; usage2 (make-div 'aaa "hello2")
 ;; usage3 (make-div :xyz "good" :cname "boojum")
 ;; usage4 (make-div :zzz "bad" :cname "stark")
-(defun make-div (id text &key (cname nil))
-  (let ((aid (intern (string id))))
-    (if cname (set aid (format nil "<div id=\"~a\" class=\"~a\">~a</div>" aid cname text))
-              (set aid (format nil "<div id=\"~a\">~a</div>" aid text)))
-  )
+
+(defun make-div0 (id text &rest attrs)
+  (tag :div (append (list :id id) attrs) text)
 )
+
+;(defun make-div (id text &rest attrs)
+;  (let ((aid (intern (string id))))
+;    (if cname (set aid (format nil "<div id=\"~a\" class=\"~a\">~a</div>" aid cname text))
+;              (set aid (format nil "<div id=\"~a\">~a</div>" aid text)))
+;  )
+;)
 
 (defun my-doc (mytitle)
   (let (d1 d2 html)
     (my-head "CSSとHTMLの例")
-    (setq d1 (make-div :line1 "これは最悪" :cname :ja))
-    (setq d2 (make-div :line2 "ファンタスティック" :cname :eja))
+    (setq d1 (make-div0 :line1 "これは最悪" :class "ja"))
+    (setq d2 (make-div0 :line2 "ファンタスティック" :class "eja"))
 
     (setq html (my-page 
                   (my-head mytitle)
